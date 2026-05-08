a8930fb15ebf3e83c08735787d88b9f0274de774
João Castro <castrojoao839@gmail.com>
2026-05-08 23:57:08 +0100

    tweaks

--- scenes/great_hall.gd
+++ scenes/great_hall.gd
@@ -1,14 +1,20 @@
 extends Node3D
 
 @export var box_scene: PackedScene
-@export var count: int = 15
+@export var count: int = 12
 @export var offset: float = 4.5
 func _ready():
 	if box_scene == null:
 		print("ERROR: missing box scene!")
 		return
 
+
 	for i in range(count):
-		var instance = box_scene.instantiate()
-		add_child(instance)
-		instance.position = Vector3(i * offset, 0, 0)
+		var instance_a = box_scene.instantiate()
+		instance_a.position = Vector3(i * offset, 0, 0)
+		add_child(instance_a)
+
+		var instance_b = box_scene.instantiate()
+		instance_b.rotation_degrees.y = 180
+		instance_b.position = Vector3(i * offset, 0, -15)
+		add_child(instance_b)
